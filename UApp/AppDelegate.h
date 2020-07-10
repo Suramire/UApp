a6b4310a32838622c6c4a6574e911a3819fd75cd
Suramire <suramire@126.com>
2020-07-10 15:00:28 +0800

    init

---
+++ UApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UApp
+//
+//  Created by Suramire on 2020/7/10.
+//  Copyright © 2020 Suramire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
